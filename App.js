37053ae2e266d286b570554f16510cf98a23b429
lpayne13 <144291432+lpayne13@users.noreply.github.com>
2023-10-06 13:51:08 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from './Counter';
+function App() {
+  return (
+        <div className='App'>
+          <h1>React Counter</h1>
+          <Counter />
+        </div>    
+  );
+}
+
+export default App;
